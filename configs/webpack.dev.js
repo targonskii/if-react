869bdc7a983c9b8458a4eb4ff44b5a72b78c9333
targonskii <polarpando@gmail.com>
2021-10-04 20:14:16 +0300

    Installed and configured webpack

---
+++ configs/webpack.dev.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+
+const commonConfig = require('./webpack.common');
+
+const source = path.resolve(__dirname, '../src');
+
+module.exports = merge(commonConfig, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 8000,
+    hot: true,
+    open: true,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: source,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+          },
+        ],
+      },
+    ],
+  },
+});
