305d2779217950f1fc24f78b2b92c158236e19a8
targonskii <polarpando@gmail.com>
2021-11-19 23:57:05 +0300

    Fixed styles of selectors

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from "react";
+import PropTypes from "prop-types";
 
-import Selector from '../Selector'
+import Selector from "../Selector";
 
 function Filter({ filterData, setFilterData }) {
   const filterDefault = {
@@ -30,151 +30,198 @@ function Filter({ filterData, setFilterData }) {
     plusChild: false,
     minusRooms: true,
     plusRooms: false,
-  })
- 
+  });
+
   const [childAge, setChildAge] = useState([]);
 
   const clickMinusAdults = () => {
     setAdults((adults) => adults - 1);
-  }
+  };
   const clickPlusAdults = () => {
     setAdults((adults) => adults + 1);
-  }
+  };
   const clickMinusChild = () => {
     setChildren((children) => children - 1);
     minusSelector();
-  }
+  };
   const clickPlusChild = () => {
     setChildren((children) => children + 1);
     plusSelector();
-  }
+  };
   const clickMinusRooms = () => {
     setRooms((rooms) => rooms - 1);
-  }
+  };
   const clickPlusRooms = () => {
     setRooms((rooms) => rooms + 1);
-  }
+  };
 
   const plusSelector = () => {
-    setChildAge([...childAge, {id: Date.now()}])
-  }
+    setChildAge([...childAge, { id: Date.now() }]);
+  };
 
   const minusSelector = () => {
-    setChildAge(childAge.slice(1))
-  }
+    setChildAge(childAge.slice(1));
+  };
 
   useEffect(() => {
     if (adults == filterDefault.adults.min) {
-      setStateButtons({ ...stateButtons, minusAdults: true })
+      setStateButtons({ ...stateButtons, minusAdults: true });
     } else if (adults == filterDefault.adults.max) {
-      setStateButtons({ ...stateButtons, plusAdults: true })
-    } else if (adults > filterDefault.adults.min || adults < filterDefault.adults.max) {
-      setStateButtons({ ...stateButtons, minusAdults: false, plusAdults: false })
+      setStateButtons({ ...stateButtons, plusAdults: true });
+    } else if (
+      adults > filterDefault.adults.min ||
+      adults < filterDefault.adults.max
+    ) {
+      setStateButtons({
+        ...stateButtons,
+        minusAdults: false,
+        plusAdults: false,
+      });
     }
 
-    setFilterData({ ...filterData, adults: adults })
-  }, [adults])
+    setFilterData({ ...filterData, adults: adults });
+  }, [adults]);
 
   useEffect(() => {
     if (children == filterDefault.children.min) {
-      setStateButtons({ ...stateButtons, minusChild: true})
+      setStateButtons({ ...stateButtons, minusChild: true });
     } else if (children == filterDefault.children.max) {
-      setStateButtons({ ...stateButtons, plusChild: true})
-    } else if (children > filterDefault.children.min || children < filterDefault.children.max) {
-      setStateButtons({ ...stateButtons, minusChild: false, plusChild: false})
+      setStateButtons({ ...stateButtons, plusChild: true });
+    } else if (
+      children > filterDefault.children.min ||
+      children < filterDefault.children.max
+    ) {
+      setStateButtons({ ...stateButtons, minusChild: false, plusChild: false });
     }
 
-    setFilterData({ ...filterData, children: children })
-  }, [children])
+    setFilterData({ ...filterData, children: children });
+  }, [children]);
 
   useEffect(() => {
     if (rooms == filterDefault.rooms.min) {
-      setStateButtons({ ...stateButtons, minusRooms: true})
+      setStateButtons({ ...stateButtons, minusRooms: true });
     } else if (rooms == filterDefault.rooms.max) {
-      setStateButtons({ ...stateButtons, plusRooms: true})
-    } else if (rooms > filterDefault.rooms.min || rooms < filterDefault.rooms.max) {
-      setStateButtons({ ...stateButtons, minusRooms: false, plusRooms: false})
+      setStateButtons({ ...stateButtons, plusRooms: true });
+    } else if (
+      rooms > filterDefault.rooms.min ||
+      rooms < filterDefault.rooms.max
+    ) {
+      setStateButtons({ ...stateButtons, minusRooms: false, plusRooms: false });
     }
 
-    setFilterData({ ...filterData, rooms: rooms })
-  }, [rooms])
+    setFilterData({ ...filterData, rooms: rooms });
+  }, [rooms]);
 
   return (
     <div className="header__people_filter">
       <div className="header__people_adults">
         <span>Adults</span>
         <div className="header__people_count">
-          <button 
-            className={(filterData.adults <= filterDefault.adults.min) ? 'header__people_disabled' : ''}
+          <button
+            className={
+              filterData.adults <= filterDefault.adults.min
+                ? "header__people_disabled"
+                : ""
+            }
             disabled={stateButtons.minusAdults}
             onClick={clickMinusAdults}
-            type="button" 
-            value="-">
-              -
+            type="button"
+            value="-"
+          >
+            -
           </button>
-          <span className="adults" name="adults">{adults}</span>
-          <button 
-            className={(filterData.adults >= filterDefault.adults.max) ? 'header__people_disabled' : ''}
+          <span className="adults" name="adults">
+            {adults}
+          </span>
+          <button
+            className={
+              filterData.adults >= filterDefault.adults.max
+                ? "header__people_disabled"
+                : ""
+            }
             disabled={stateButtons.plusAdults}
             onClick={clickPlusAdults}
-            type="button" 
-            value="+">
-              +
+            type="button"
+            value="+"
+          >
+            +
           </button>
         </div>
       </div>
       <div className="header__people_children">
         <span>Children</span>
         <div className="header__people_count">
-          <button 
-            className={(filterData.children <= filterDefault.children.min) ? 'header__people_disabled' : ''}
+          <button
+            className={
+              filterData.children <= filterDefault.children.min
+                ? "header__people_disabled"
+                : ""
+            }
             disabled={stateButtons.minusChild}
             onClick={clickMinusChild}
-            type="button" 
-            value="-">
-              -
+            type="button"
+            value="-"
+          >
+            -
           </button>
           <span className="children">{children}</span>
-          <button 
-            className={(filterData.children >= filterDefault.children.max) ? 'header__people_disabled' : ''}
+          <button
+            className={
+              filterData.children >= filterDefault.children.max
+                ? "header__people_disabled"
+                : ""
+            }
             disabled={stateButtons.plusChild}
             onClick={clickPlusChild}
-            type="button" 
-            value="+">
-              +
+            type="button"
+            value="+"
+          >
+            +
           </button>
         </div>
       </div>
       <div className="header__people_rooms">
         <span>Rooms</span>
         <div className="header__people_count">
-          <button 
-            className={(filterData.rooms <= filterDefault.rooms.min) ? 'header__people_disabled' : ''}
+          <button
+            className={
+              filterData.rooms <= filterDefault.rooms.min
+                ? "header__people_disabled"
+                : ""
+            }
             disabled={stateButtons.minusRooms}
-            onClick={clickMinusRooms} 
-            type="button" 
-            value="-">
-              -
+            onClick={clickMinusRooms}
+            type="button"
+            value="-"
+          >
+            -
           </button>
-          <span className="rooms" name="rooms">{rooms}</span>
-          <button 
-            className={(filterData.rooms >= filterDefault.rooms.max) ? 'header__people_disabled' : ''}
+          <span className="rooms" name="rooms">
+            {rooms}
+          </span>
+          <button
+            className={
+              filterData.rooms >= filterDefault.rooms.max
+                ? "header__people_disabled"
+                : ""
+            }
             disabled={stateButtons.plusRooms}
-            onClick={clickPlusRooms} 
-            type="button" 
-            value="+">
-              +
+            onClick={clickPlusRooms}
+            type="button"
+            value="+"
+          >
+            +
           </button>
         </div>
       </div>
-      {childAge?.length >0 && <Selector childAge={childAge} />}
+      {childAge?.length > 0 && <Selector childAge={childAge} />}
     </div>
   );
-};
+}
 
 export default Filter;
 
 Filter.propTypes = {
   setFilterData: PropTypes.func.isRequired,
-  filterData: PropTypes.object.isRequired
+  filterData: PropTypes.object.isRequired,
 };
